a198bac9b54262b0d2b75fadf24696d00607ee16
Gabriele Genta <gabriele.genta@gmail.com>
2021-07-30 15:26:11 +0200

    Add toggle feed button

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,17 @@ import './App.css'
 import patchLevels from './lib/patchLevels'
 import { OrderBookLevel } from './types'
 
+type ProductId = 'PI_XBTUSD' | 'PI_ETHUSD'
+
 type SummaryEventData = {
   numLevels: number
-  product_id: string
+  product_id: ProductId
   bids: [number, number][]
   asks: [number, number][]
 }
 
 type DeltaEventData = {
-  product_id: string
+  product_id: ProductId
   bids: [number, number][]
   asks: [number, number][]
 }
@@ -26,7 +28,7 @@ type WebsocketEventData =
   | {
       event: 'subscribed'
       feed: string
-      product_ids: string[]
+      product_ids: ProductId[]
     }
   | SummaryEventData
   | DeltaEventData
@@ -42,7 +44,14 @@ const rawLevelToOrderBookLevel = ([price, size]: [
 ]): OrderBookLevel => ({ price, size })
 
 function App() {
+  const [productId, setProductId] = useState<ProductId>('PI_XBTUSD')
   const [book, setBook] = useState<OrderBook>()
+
+  const handleToggleFeedClick = () => {
+    setProductId((currentProductId) =>
+      currentProductId === 'PI_XBTUSD' ? 'PI_ETHUSD' : 'PI_XBTUSD'
+    )
+  }
 
   useEffect(() => {
     const ws = new WebSocket('wss://www.cryptofacilities.com/ws/v1')
@@ -53,7 +62,7 @@ function App() {
         JSON.stringify({
           event: 'subscribe',
           feed: 'book_ui_1',
-          product_ids: ['PI_XBTUSD'],
+          product_ids: [productId],
         })
       )
     })
@@ -106,7 +115,7 @@ function App() {
     }, 16) // 16ms = ~60 fps
 
     return () => ws.close()
-  }, [])
+  }, [productId])
 
   return (
     <div className="App">
@@ -133,6 +142,7 @@ function App() {
           </ul>
         </>
       )}
+      <button onClick={handleToggleFeedClick}>Toggle feed</button>
     </div>
   )
 }
